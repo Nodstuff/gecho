5487b4638fcf553d33b2b527b74b1b8e6d70cd71
Tom Meaney <t.meaney@f5.com>
2023-02-21 22:45:26 +0000

    tidy up

--- main.go
+++ main.go
@@ -20,22 +20,22 @@ func main() {
 	}
 }
 
-func handleEcho() http.Handler {
+func echoHandler() http.Handler {
 	return http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
 		rw.Header().Set("Content-Type", "application/json")
-		data, err := json.Marshal(buildResponseBody(req))
+		d, err := json.Marshal(buildResponseBody(req))
 		if err != nil {
 			log.Println(err)
 		}
-		if _, err = io.Copy(rw, bytes.NewReader(data)); err != nil {
+		if _, err = io.Copy(rw, bytes.NewReader(d)); err != nil {
 			log.Println(err)
 		}
 	})
 }
 
-func buildResponseBody(req *http.Request) map[string]any {
+func buildResponseBody(r *http.Request) map[string]any {
 	rbm := make(map[string]any)
-	rb, err := io.ReadAll(req.Body)
+	rb, err := io.ReadAll(r.Body)
 	if err != nil {
 		log.Println(err)
 	}
@@ -45,60 +45,60 @@ func buildResponseBody(req *http.Request) map[string]any {
 	}
 	b := make(map[string]any)
 	b["statusBody"] = "Healthy"
-	b["statusReason"] = fmt.Sprintf("Incoming request was on port %s", getPort(req.Host, req.TLS))
-	b["hostname"] = req.Host
-	b["uri"] = buildURIResponse(req)
-	b["network"] = buildNetworkResponse(req)
-	b["ssl"] = buildSSLResponse(req)
-	b["requestHeaders"] = buildRequestHeadersResponse(req)
-	b["session"] = buildSessionResponse(req)
+	b["statusReason"] = fmt.Sprintf("Incoming request was on port %s", getPort(r.Host, r.TLS))
+	b["hostname"] = r.Host
+	b["uri"] = buildURI(r)
+	b["network"] = buildNetwork(r)
+	b["ssl"] = buildSSL(r)
+	b["requestHeaders"] = buildRequestHeaders(r)
+	b["session"] = buildSession(r)
 	b["body"] = rbm
-	b["statusCode"] = 200
+	b["statusCode"] = http.StatusOK
 	return b
 }
 
-func buildRequestHeadersResponse(req *http.Request) map[string]any {
+func buildRequestHeaders(r *http.Request) map[string]any {
 	h := make(map[string]any)
-	for key, values := range req.Header {
-		for _, value := range values {
-			h[key] = value
+	for k, vs := range r.Header {
+		for _, v := range vs {
+			h[k] = v
 		}
 	}
 	return h
 }
 
-func buildURIResponse(req *http.Request) map[string]any {
+func buildURI(r *http.Request) map[string]any {
 	u := make(map[string]any)
-	u["httpVersion"] = req.Proto
-	u["method"] = req.Method
-	u["scheme"] = getScheme(req.TLS)
-	u["fullPath"] = req.URL.Path
-	u["queryString"] = req.URL.Query().Encode()
+	u["httpVersion"] = r.Proto
+	u["method"] = r.Method
+	u["scheme"] = getScheme(r.TLS)
+	u["fullPath"] = r.URL.Path
+	u["queryString"] = r.URL.Query().Encode()
 	return u
 }
 
-func buildNetworkResponse(req *http.Request) map[string]any {
+func buildNetwork(r *http.Request) map[string]any {
 	n := make(map[string]any)
-	n["clientPort"] = getPort(req.RemoteAddr, nil)
-	n["serverPort"] = getPort(req.Host, req.TLS)
-	n["serverAddress"] = req.Host
-	n["clientAddress"] = req.RemoteAddr
+	n["clientPort"] = getPort(r.RemoteAddr, nil)
+	n["serverPort"] = getPort(r.Host, r.TLS)
+	n["serverAddress"] = r.Host
+	n["clientAddress"] = r.RemoteAddr
 	return n
 }
 
-func buildSessionResponse(req *http.Request) map[string]any {
+func buildSession(r *http.Request) map[string]any {
 	s := make(map[string]any)
-	s["cookie"] = req.Cookies()
+	s["cookie"] = r.Cookies()
 	return s
 }
 
-func buildSSLResponse(req *http.Request) map[string]any {
+func buildSSL(r *http.Request) map[string]any {
 	s := make(map[string]any)
-	if req.TLS != nil {
-		s["negotiatedProtocol"] = req.TLS.NegotiatedProtocol
-		s["cipherSuite"] = req.TLS.CipherSuite
-		s["serverName"] = req.TLS.ServerName
-		s["version"] = req.TLS.Version
+	if r.TLS != nil {
+		s["negotiatedProtocol"] = r.TLS.NegotiatedProtocol
+		s["cipherSuite"] = r.TLS.CipherSuite
+		s["serverName"] = r.TLS.ServerName
+		s["version"] = r.TLS.Version
 	}
 	return s
 }
@@ -110,39 +110,38 @@ func getScheme(s *tls.ConnectionState) string {
 	return "https"
 }
 
-func getPort(a string, t *tls.ConnectionState) string {
-	if t != nil {
-		return "443"
-	}
+func getPort(a string, s *tls.ConnectionState) string {
 	_, p, _ := net.SplitHostPort(a)
-	if p == "" {
-		p = "80"
+	if p == "" && s != nil {
+		return "443"
+	} else if p == "" {
+		return "80"
 	}
 	return p
 }
 
+func fileExists(f string) bool {
+	i, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !i.IsDir()
+}
+
 func startInsecureOnly() {
-	if err := http.ListenAndServe(":8080", handleEcho()); err != nil {
+	if err := http.ListenAndServe(":8080", echoHandler()); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func startSecureAndInsecure() {
 	go func() {
-		if err := http.ListenAndServe(":8080", handleEcho()); err != nil {
+		if err := http.ListenAndServe(":8080", echoHandler()); err != nil {
 			log.Fatal(err)
 		}
 	}()
 
-	if err := http.ListenAndServeTLS(":8443", "./ssl/certs/server.crt", "./ssl/certs/server.key", handleEcho()); err != nil {
+	if err := http.ListenAndServeTLS(":8443", "./ssl/certs/server.crt", "./ssl/certs/server.key", echoHandler()); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func fileExists(filename string) bool {
-	info, err := os.Stat(filename)
-	if os.IsNotExist(err) {
-		return false
-	}
-	return !info.IsDir()
 }
